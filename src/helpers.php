f43112236c095b3e69b452a60d33d2664d4fed3a
Rahul Kadyan <hi@znck.me>
2016-10-04 15:06:24 +0000

    Applied fixes from StyleCI

--- src/helpers.php
+++ src/helpers.php
@@ -8,8 +8,9 @@ use Znck\Attach\Downloader;
 use Znck\Attach\Processors\Resize;
 use Znck\Attach\Processors\SaveIfDirty;
 
-if (!function_exists('make_image_uploader')) {
-    function make_image_uploader(Request $request, string $key = 'file') : Uploader {
+if (! function_exists('make_image_uploader')) {
+    function make_image_uploader(Request $request, string $key = 'file') : Uploader
+    {
         $attachment = app(Attachment::class);
         $uploader = app(Uploader::class, [$request->file($key), $attachment, false]);
         $attachment->fill($request->input());
@@ -21,8 +22,9 @@ if (!function_exists('make_image_uploader')) {
     }
 }
 
-if (!function_exists('make_file_uploader')) {
-    function make_file_uploader(Request $request, string $key = 'file') : Uploader {
+if (! function_exists('make_file_uploader')) {
+    function make_file_uploader(Request $request, string $key = 'file') : Uploader
+    {
         $attachment = app(Attachment::class);
         $uploader = app(Uploader::class, [$request->file($key), $attachment]);
         $attachment->fill($request->input());
@@ -32,20 +34,23 @@ if (!function_exists('make_file_uploader')) {
     }
 }
 
-if (!function_exists('serve_attachment')) {
-    function serve_attachment(string $filename) : Response {
+if (! function_exists('serve_attachment')) {
+    function serve_attachment(string $filename) : Response
+    {
         return (new Downloader())->response($filename);
     }
 }
 
-if (!function_exists('download_attachment')) {
-    function download_attachment(string $filename) : Response {
+if (! function_exists('download_attachment')) {
+    function download_attachment(string $filename) : Response
+    {
         return (new Downloader())->download($filename);
     }
 }
 
-if (!function_exists('sign_url')) {
-    function sign_url(string $url, $extras = [], int $expiry = null) {
+if (! function_exists('sign_url')) {
+    function sign_url(string $url, $extras = [], int $expiry = null)
+    {
         ksort($extras);
 
         $expiry = is_null($expiry) ? config('attach.signing.expiry') : $expiry;
@@ -63,8 +68,9 @@ if (!function_exists('sign_url')) {
     }
 }
 
-if (!function_exists('verify_url_signature')) {
-    function verify_url_signature(string $url, string $signature, $expiry = 0) {
+if (! function_exists('verify_url_signature')) {
+    function verify_url_signature(string $url, string $signature, $expiry = 0)
+    {
         $secret = config('attach.signing.key');
 
         $query = parse_url($url, PHP_URL_QUERY);
@@ -81,8 +87,9 @@ if (!function_exists('verify_url_signature')) {
     }
 }
 
-if (!function_exists('attach_url')) {
-    function attach_url(Attachment $attachment, string $var = null, $params = [], bool $sign = null) {
+if (! function_exists('attach_url')) {
+    function attach_url(Attachment $attachment, string $var = null, $params = [], bool $sign = null)
+    {
         $route = config('attach.route');
         $sign = is_null($sign) ? config('attach.sign') : $sign;
         $name = is_string($route) ? $route : array_get($route, 'as');
