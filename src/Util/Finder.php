7d79d0c2a8054637a5f907dde9b3fecbf97c8b34
Rahul Kadyan <hi@znck.me>
2016-12-07 19:06:54 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Util/Finder.php
+++ src/Util/Finder.php
@@ -1,7 +1,7 @@
 <?php namespace Znck\Attach\Util;
 
-use Illuminate\Filesystem\FilesystemManager;
 use Illuminate\Contracts\Filesystem\Filesystem;
+use Illuminate\Filesystem\FilesystemManager;
 use League\Flysystem\FilesystemInterface;
 use Znck\Attach\Contracts\Attachment;
 use Znck\Attach\Contracts\Finder as FinderInterface;
@@ -10,7 +10,8 @@ class Finder implements FinderInterface
 {
     protected $storage;
 
-    public function getStorage() : Filesystem {
+    public function getStorage() : Filesystem
+    {
         if (is_null($this->storage)) {
             $this->storage = app(Storage::class);
         }
@@ -18,15 +19,18 @@ class Finder implements FinderInterface
         return $this->storage;
     }
 
-    public function setStorage(Filesystem $storage) {
+    public function setStorage(Filesystem $storage)
+    {
         $this->storage = $storage;
     }
 
-    public function get(string $path) {
+    public function get(string $path)
+    {
         return $this->getStorage()->get($path);
     }
 
-    public function put(string $path, $content, $visibility = null) {
+    public function put(string $path, $content, $visibility = null)
+    {
         return $this->getStorage()->put($path, $content, $visibility);
     }
 
@@ -35,26 +39,30 @@ class Finder implements FinderInterface
      *
      * @return resource|false
      */
-    public function readStream(string $path) {
+    public function readStream(string $path)
+    {
         return $this->getStorageDriver()->readStream($path);
     }
 
-    public function useDisk(string $disk = null) : FinderInterface {
+    public function useDisk(string $disk = null) : FinderInterface
+    {
         $this->setStorage(app(FilesystemManager::class)->disk($disk));
 
         return $this;
     }
 
-
-    public function getStorageDriver() : FilesystemInterface {
+    public function getStorageDriver() : FilesystemInterface
+    {
         return $this->getStorage()->getDriver();
     }
 
-    public function size(string $path) {
+    public function size(string $path)
+    {
         return $this->getStorage()->size($path);
     }
 
-    public function getPath(Attachment $attachment, string $variation) : string {
+    public function getPath(Attachment $attachment, string $variation) : string
+    {
         $path = $attachment->path;
         $extension = $attachment->extension;
         $path = str_replace_last($extension, '', $path);
