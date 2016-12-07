da515087b1d2ab36fdb94fd5f4c761fa92e3e692
Rahul Kadyan <hi@znck.me>
2016-12-07 11:03:15 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Contracts/Signer.php
+++ src/Contracts/Signer.php
@@ -5,25 +5,25 @@ namespace Znck\Attach\Contracts;
 interface Signer
 {
     /**
-     * Create a signed url
+     * Create a signed url.
      *
-     * @param  string $url          Given url.
-     * @param  int|null $expiry     Expired at timestamp.
-     * @param  bool $ignoreParams   Ignore params.
+     * @param string   $url          Given url.
+     * @param int|null $expiry       Expired at timestamp.
+     * @param bool     $ignoreParams Ignore params.
      *
-     * @return string               Signed url.
+     * @return string Signed url.
      */
     public function sign(string $url, int $expiry = null, bool $ignoreParams = true): string;
 
     /**
-     * Verify url signature
+     * Verify url signature.
      *
-     * @param  string $url              Signed url.
-     * @param  string $signature        Given signature.
-     * @param  int|null $expiry         Expired at timestamp.
-     * @param  bool|array $ignoreParams Ignore params.
+     * @param string     $url          Signed url.
+     * @param string     $signature    Given signature.
+     * @param int|null   $expiry       Expired at timestamp.
+     * @param bool|array $ignoreParams Ignore params.
      *
-     * @return bool                     True if valid.
+     * @return bool True if valid.
      */
     public function verify(string $url, string $signature, int $expiry = null, $ignoreParams = true): bool;
 }
